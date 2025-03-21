1102287a94368fdb5dcace956e6d9fa1e3e40df4
fahim0509 <anik.fahim07@gmail.com>
2025-03-21 12:16:10 +0600

    basic data-type problem 2

---
+++ basic-datatype.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  long long m;
+  char c;
+  float f;
+  double d;
+  cin >> n >> m >> c >> f >> d;
+    cout << n << endl; 
+    cout << m << endl;
+    cout << c << endl;
+    cout << f << endl;
+    cout << d << endl;
+    return 0;
+    
+}
