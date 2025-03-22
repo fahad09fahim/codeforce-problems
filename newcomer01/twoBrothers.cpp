af67c11b3423c1cbb9ea136d5971c39aec64eb6f
fahim0509 <anik.fahim07@gmail.com>
2025-03-22 23:58:19 +0600

    the brothers

---
+++ newcomer01/twoBrothers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string firstA, firstB, secondA, secondB;
+    cin >> firstA >> firstB  ;
+    cin >> secondA >> secondB  ;
+
+    if(firstB == secondB){
+        cout << "ARE Brothers";
+    }else{
+        cout << "NOT";
+    }
+
+
+
+}
